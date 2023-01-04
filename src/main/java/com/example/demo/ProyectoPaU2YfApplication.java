de36f7722d00d1c4515d1fd3ff65e6e710a975e8
YaniryFlorez <ymflorezrivera@gmail.com>
2023-01-03 19:46:07 -0500

    taller17 conexion database

---
+++ src/main/java/com/example/demo/ProyectoPaU2YfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2YfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2YfApplication.class, args);
+	}
+
+}
